cadaa68bf289f5b01a3a47242cfd5cfec3fa94a4
김태중 <95677522+taejoong15@users.noreply.github.com>
2026-04-09 23:41:01 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.println((A+B) % C);
+        System.out.println(((A%C) + (B%C)) % C);
+        System.out.println((A*B) % C);
+        System.out.println((A%C) * (B%C) % C);
+        
+        
+    }
+}
